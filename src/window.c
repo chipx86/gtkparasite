5a982d3d086991e63edb2487bb8d1bd008566778
chipx86 <chipx86@87a1d21d-fa52-0410-82b2-83ba5e1e7780>
2008-07-27 09:48:45 +0000

    Show the application name in the titlebar so we can distinguish between parasites.
    git-svn-id: https://gtkparasite.googlecode.com/svn/trunk/gtkparasite@10 87a1d21d-fa52-0410-82b2-83ba5e1e7780

--- src/window.c
+++ src/window.c
@@ -82,6 +82,7 @@ gtkparasite_window_create()
     ParasiteWindow *window;
     GtkWidget *main_vbox;
     GtkWidget *vpaned;
+    char *title;
 
     window = g_new0(ParasiteWindow, 1);
 
@@ -90,8 +91,11 @@ gtkparasite_window_create()
      */
     window->window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_default_size(GTK_WINDOW(window->window), 1000, 500);
-    gtk_window_set_title(GTK_WINDOW(window->window), "Parasite");
     gtk_widget_show(window->window);
+
+    title = g_strdup_printf("Parasite - %s", g_get_application_name());
+    gtk_window_set_title(GTK_WINDOW(window->window), title);
+    g_free(title);
 
     main_vbox = gtk_vbox_new(FALSE, 0);
     gtk_widget_show(main_vbox);
