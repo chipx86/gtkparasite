184dc8fd44557ec25f1d71a40eb53435cc3a0ab5
Christian Hammond <chipx86@chipx86.com>
2009-01-13 15:34:27 -0800

    Move away from a GtkEntry. We're going to do all input handling in the GtkTextView.

--- src/python-shell.c
+++ src/python-shell.c
@@ -1,3 +1,5 @@
+#include <gdk/gdkkeysyms.h>
+
 #include <dlfcn.h>
 #include <Python.h>
 #include <pygobject.h>
@@ -11,8 +13,13 @@
 
 typedef struct
 {
-    GtkWidget *entry;
     GtkWidget *textview;
+
+    GtkTextMark *scroll_mark;
+    GtkTextMark *line_start_mark;
+
+    int history_pos;
+    int cursor_pos;
 
 } GtkParasitePythonShellPrivate;
 
@@ -22,13 +29,20 @@ enum
 };
 
 
+/* Widget functions */
 static void gtkparasite_python_shell_finalize(GObject *obj);
+
+/* Python integration */
 static void gtkparasite_python_shell_handle_stdout(GtkWidget *python_shell,
                                                    const char *str);
 static void gtkparasite_python_shell_handle_stderr(GtkWidget *python_shell,
                                                    const char *str);
-static void gtkparasite_python_shell_entry_activate_cb(GtkWidget *entry,
-                                                       GtkWidget *python_shell);
+static char *gtkparasite_python_shell_get_input(GtkWidget *python_shell);
+
+/* Callbacks */
+static gboolean gtkparasite_python_shell_key_press_cb(GtkWidget *textview,
+                                                      GdkEventKey *event,
+                                                      GtkWidget *python_shell);
 
 
 static GtkVBoxClass *parent_class = NULL;
@@ -38,7 +52,7 @@ static GtkWidget *cur_python_shell = NULL;
 G_DEFINE_TYPE(GtkParasitePythonShell, gtkparasite_python_shell, GTK_TYPE_VBOX);
 
 
-PyObject *
+static PyObject *
 capture_stdout(PyObject *self, PyObject *args)
 {
     char *str = NULL;
@@ -53,7 +67,7 @@ capture_stdout(PyObject *self, PyObject *args)
     return Py_None;
 }
 
-PyObject *
+static PyObject *
 capture_stderr(PyObject *self, PyObject *args)
 {
     char *str = NULL;
@@ -68,7 +82,7 @@ capture_stderr(PyObject *self, PyObject *args)
     return Py_None;
 }
 
-PyObject *
+static PyObject *
 wrap_gobj(PyObject *self, PyObject *args)
 {
     void *addr;
@@ -151,8 +165,8 @@ gtkparasite_python_shell_init(GtkParasitePythonShell *python_shell)
     GtkParasitePythonShellPrivate *priv =
         GTKPARASITE_PYTHON_SHELL_GET_PRIVATE(python_shell);
     GtkWidget *swin;
-    GtkWidget *hbox;
-    GtkWidget *label;
+    GtkTextBuffer *buffer;
+    GtkTextIter iter;
     PangoFontDescription *font_desc;
 
     gtkparasite_python_shell_init_python();
@@ -170,33 +184,28 @@ gtkparasite_python_shell_init(GtkParasitePythonShell *python_shell)
     priv->textview = gtk_text_view_new();
     gtk_widget_show(priv->textview);
     gtk_container_add(GTK_CONTAINER(swin), priv->textview);
-    gtk_text_view_set_cursor_visible(GTK_TEXT_VIEW(priv->textview), FALSE);
-    gtk_text_view_set_editable(GTK_TEXT_VIEW(priv->textview), FALSE);
+    gtk_text_view_set_cursor_visible(GTK_TEXT_VIEW(priv->textview), TRUE);
 
+    g_signal_connect(G_OBJECT(priv->textview), "key_press_event",
+                     G_CALLBACK(gtkparasite_python_shell_key_press_cb),
+                     python_shell);
+
+    /* Make the textview monospaced */
     font_desc = pango_font_description_from_string("monospace");
     pango_font_description_set_size(font_desc, 8 * PANGO_SCALE);
     gtk_widget_modify_font(priv->textview, font_desc);
-
-    hbox = gtk_hbox_new(FALSE, 6);
-    gtk_widget_show(hbox);
-    gtk_box_pack_start(GTK_BOX(python_shell), hbox, FALSE, FALSE, 0);
-
-    label = gtk_label_new_with_mnemonic("_Python: ");
-    gtk_widget_show(label);
-    gtk_box_pack_start(GTK_BOX(hbox), label, FALSE, FALSE, 0);
-
-    priv->entry = gtk_entry_new();
-    gtk_widget_show(priv->entry);
-    gtk_box_pack_start(GTK_BOX(hbox), priv->entry, TRUE, TRUE, 0);
-    gtk_widget_modify_font(priv->entry, font_desc);
-
     pango_font_description_free(font_desc);
 
-    g_signal_connect(G_OBJECT(priv->entry), "activate",
-                     G_CALLBACK(gtkparasite_python_shell_entry_activate_cb),
-                     python_shell);
+    /* Create the end-of-buffer mark */
+    buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(priv->textview));
+    gtk_text_buffer_get_end_iter(buffer, &iter);
+    priv->scroll_mark = gtk_text_buffer_create_mark(buffer, "scroll_mark",
+                                                    &iter, FALSE);
 
-    // TODO: Set the font
+    /* Create the beginning-of-line mark */
+    priv->line_start_mark = gtk_text_buffer_create_mark(buffer,
+                                                        "line_start_mark",
+                                                        &iter, TRUE);
 }
 
 static void
@@ -239,16 +248,112 @@ gtkparasite_python_shell_handle_stderr(GtkWidget *python_shell,
 }
 
 static void
-gtkparasite_python_shell_entry_activate_cb(GtkWidget *entry,
-                                           GtkWidget *python_shell)
+gtkparasite_python_shell_process_line(GtkWidget *python_shell)
 {
-    const char *command = gtk_entry_get_text(GTK_ENTRY(entry));
+    GtkParasitePythonShellPrivate *priv =
+        GTKPARASITE_PYTHON_SHELL_GET_PRIVATE(python_shell);
 
+    char *command = gtkparasite_python_shell_get_input(python_shell);
+
+    gtkparasite_python_shell_append_text(python_shell, "\n");
+
+    priv->history_pos = 0;
     cur_python_shell = python_shell;
     PyRun_SimpleString(command);
     cur_python_shell = NULL;
 
-    gtk_entry_set_text(GTK_ENTRY(entry), "");
+    g_free(command);
+}
+
+static char *
+gtkparasite_python_shell_get_input(GtkWidget *python_shell)
+{
+    GtkParasitePythonShellPrivate *priv =
+        GTKPARASITE_PYTHON_SHELL_GET_PRIVATE(python_shell);
+    GtkTextBuffer *buffer =
+        gtk_text_view_get_buffer(GTK_TEXT_VIEW(priv->textview));
+    GtkTextIter start_iter;
+    GtkTextIter end_iter;
+
+    gtk_text_buffer_get_iter_at_mark(buffer, &start_iter,
+                                     priv->line_start_mark);
+    gtk_text_buffer_get_end_iter(buffer, &end_iter);
+
+    return gtk_text_buffer_get_slice(buffer, &start_iter, &end_iter, FALSE);
+}
+
+static gboolean
+gtkparasite_python_shell_key_press_cb(GtkWidget *textview,
+                                      GdkEventKey *event,
+                                      GtkWidget *python_shell)
+{
+    if (event->keyval == GDK_Return)
+    {
+        gtkparasite_python_shell_process_line(python_shell);
+        return TRUE;
+    }
+    else if (event->keyval == GDK_Up)
+    {
+#if 0
+        gtkparasite_python_shell_replace_input(python_shell,
+            gtkparasite_python_shell_get_history_back(python_shell));
+#endif
+        return TRUE;
+    }
+    else if (event->keyval == GDK_Down)
+    {
+#if 0
+        gtkparasite_python_shell_replace_input(python_shell,
+            gtkparasite_python_shell_get_history_forward(python_shell));
+#endif
+        return TRUE;
+    }
+    else if (event->string != NULL)
+    {
+        GtkParasitePythonShellPrivate *priv =
+            GTKPARASITE_PYTHON_SHELL_GET_PRIVATE(python_shell);
+        GtkTextBuffer *buffer =
+            gtk_text_view_get_buffer(GTK_TEXT_VIEW(priv->textview));
+        GtkTextMark *insert_mark = gtk_text_buffer_get_insert(buffer);
+        GtkTextMark *selection_mark =
+            gtk_text_buffer_get_selection_bound(buffer);
+        GtkTextIter insert_iter;
+        GtkTextIter selection_iter;
+        GtkTextIter start_iter;
+        gint cmp_start_insert;
+        gint cmp_start_select;
+        gint cmp_insert_select;
+
+        gtk_text_buffer_get_iter_at_mark(buffer, &start_iter,
+                                         priv->line_start_mark);
+        gtk_text_buffer_get_iter_at_mark(buffer, &insert_iter, insert_mark);
+        gtk_text_buffer_get_iter_at_mark(buffer, &selection_iter,
+                                         selection_mark);
+
+        cmp_start_insert = gtk_text_iter_compare(&start_iter, &insert_iter);
+        cmp_start_select = gtk_text_iter_compare(&start_iter, &selection_iter);
+        cmp_insert_select = gtk_text_iter_compare(&insert_iter,
+                                                  &selection_iter);
+
+        if (cmp_start_insert <= 0 && cmp_start_select <= 0)
+        {
+            return FALSE;
+        }
+        else if (cmp_start_insert > 0 && cmp_start_select > 0)
+        {
+            gtk_text_buffer_place_cursor(buffer, &start_iter);
+        }
+        else if (cmp_insert_select < 0)
+        {
+            gtk_text_buffer_move_mark(buffer, insert_mark, &start_iter);
+        }
+        else if (cmp_insert_select > 0)
+        {
+            gtk_text_buffer_move_mark(buffer, selection_mark, &start_iter);
+        }
+    }
+
+    return FALSE;
 }
 
 GtkWidget *
