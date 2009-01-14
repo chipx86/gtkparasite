8751e343774e7856fcf8cc0012574926183f7f41
Christian Hammond <chipx86@chipx86.com>
2009-01-13 17:26:00 -0800

    Implement history for the prompt.

--- src/python-shell.c
+++ src/python-shell.c
@@ -3,6 +3,8 @@
 #include "python-hooks.h"
 #include "python-shell.h"
 
+#define MAX_HISTORY_LENGTH 20
+
 #define GTKPARASITE_PYTHON_SHELL_GET_PRIVATE(obj) \
     (G_TYPE_INSTANCE_GET_PRIVATE((obj), GTKPARASITE_TYPE_PYTHON_SHELL, \
                                  GtkParasitePythonShellPrivate))
@@ -14,8 +16,8 @@ typedef struct
     GtkTextMark *scroll_mark;
     GtkTextMark *line_start_mark;
 
-    int history_pos;
-    int cursor_pos;
+    GQueue *history;
+    GList *cur_history_item;
 
 } GtkParasitePythonShellPrivate;
 
@@ -66,6 +68,8 @@ gtkparasite_python_shell_init(GtkParasitePythonShell *python_shell)
     GtkTextIter iter;
     PangoFontDescription *font_desc;
 
+    priv->history = g_queue_new();
+
     gtk_box_set_spacing(GTK_BOX(python_shell), 6);
 
     swin = gtk_scrolled_window_new(NULL, NULL);
@@ -80,6 +84,9 @@ gtkparasite_python_shell_init(GtkParasitePythonShell *python_shell)
     gtk_widget_show(priv->textview);
     gtk_container_add(GTK_CONTAINER(swin), priv->textview);
     gtk_text_view_set_cursor_visible(GTK_TEXT_VIEW(priv->textview), TRUE);
+    gtk_text_view_set_pixels_above_lines(GTK_TEXT_VIEW(priv->textview), 3);
+    gtk_text_view_set_left_margin(GTK_TEXT_VIEW(priv->textview), 3);
+    gtk_text_view_set_right_margin(GTK_TEXT_VIEW(priv->textview), 3);
 
     g_signal_connect(G_OBJECT(priv->textview), "key_press_event",
                      G_CALLBACK(gtkparasite_python_shell_key_press_cb),
@@ -106,6 +113,7 @@ gtkparasite_python_shell_init(GtkParasitePythonShell *python_shell)
     gtk_text_buffer_create_tag(buffer, "stdout", NULL);
     gtk_text_buffer_create_tag(buffer, "stderr",
                                "foreground", "red",
+                               "paragraph-background", "#FFFFE0",
                                NULL);
     gtk_text_buffer_create_tag(buffer, "prompt",
                                "foreground", "blue",
@@ -115,8 +123,12 @@ gtkparasite_python_shell_init(GtkParasitePythonShell *python_shell)
 }
 
 static void
-gtkparasite_python_shell_finalize(GObject *obj)
+gtkparasite_python_shell_finalize(GObject *python_shell)
 {
+    GtkParasitePythonShellPrivate *priv =
+        GTKPARASITE_PYTHON_SHELL_GET_PRIVATE(python_shell);
+
+    g_queue_free(priv->history);
 }
 
 static void
@@ -167,23 +179,58 @@ gtkparasite_python_shell_write_prompt(GtkWidget *python_shell)
 
     gtk_text_buffer_get_end_iter(buffer, &iter);
     gtk_text_buffer_move_mark(buffer, priv->line_start_mark, &iter);
-    priv->history_pos = 0;
 }
 
 static void
 gtkparasite_python_shell_process_line(GtkWidget *python_shell)
 {
+    GtkParasitePythonShellPrivate *priv =
+        GTKPARASITE_PYTHON_SHELL_GET_PRIVATE(python_shell);
+
     char *command = gtkparasite_python_shell_get_input(python_shell);
 
     gtkparasite_python_shell_append_text(python_shell, "\n", NULL);
-    gtkparasite_python_run(command,
-                           gtkparasite_python_shell_log_stdout,
-                           gtkparasite_python_shell_log_stderr,
-                           python_shell);
 
-    g_free(command);
+    if (*command == '\0')
+    {
+        // The line is empty. Don't store it in the history or process it.
+        g_free(command);
+    }
+    else
+    {
+        g_queue_push_head(priv->history, command);
+        priv->cur_history_item = NULL;
+
+        if (g_queue_get_length(priv->history) > MAX_HISTORY_LENGTH)
+            g_free(g_queue_pop_tail(priv->history));
+
+        gtkparasite_python_run(command,
+                               gtkparasite_python_shell_log_stdout,
+                               gtkparasite_python_shell_log_stderr,
+                               python_shell);
+    }
 
     gtkparasite_python_shell_write_prompt(python_shell);
+}
+
+static void
+gtkparasite_python_shell_replace_input(GtkWidget *python_shell,
+                                       const char *text)
+{
+    GtkParasitePythonShellPrivate *priv =
+        GTKPARASITE_PYTHON_SHELL_GET_PRIVATE(python_shell);
+
+    GtkTextBuffer *buffer =
+        gtk_text_view_get_buffer(GTK_TEXT_VIEW(priv->textview));
+    GtkTextIter start_iter;
+    GtkTextIter end_iter;
+
+    gtk_text_buffer_get_iter_at_mark(buffer, &start_iter,
+                                     priv->line_start_mark);
+    gtk_text_buffer_get_end_iter(buffer, &end_iter);
+
+    gtk_text_buffer_delete(buffer, &start_iter, &end_iter);
+    gtk_text_buffer_insert(buffer, &end_iter, text, -1);
 }
 
 static char *
@@ -200,7 +247,43 @@ gtkparasite_python_shell_get_input(GtkWidget *python_shell)
                                      priv->line_start_mark);
     gtk_text_buffer_get_end_iter(buffer, &end_iter);
 
-    return gtk_text_buffer_get_slice(buffer, &start_iter, &end_iter, FALSE);
+    return gtk_text_buffer_get_text(buffer, &start_iter, &end_iter, FALSE);
+}
+
+static const char *
+gtkparasite_python_shell_get_history_back(GtkWidget *python_shell)
+{
+    GtkParasitePythonShellPrivate *priv =
+        GTKPARASITE_PYTHON_SHELL_GET_PRIVATE(python_shell);
+
+    if (priv->cur_history_item == NULL)
+    {
+        priv->cur_history_item = g_queue_peek_head_link(priv->history);
+
+        if (priv->cur_history_item == NULL)
+            return "";
+    }
+    else if (priv->cur_history_item->next != NULL)
+        priv->cur_history_item = priv->cur_history_item->next;
+
+    return (const char *)priv->cur_history_item->data;
+}
+
+static const char *
+gtkparasite_python_shell_get_history_forward(GtkWidget *python_shell)
+{
+    GtkParasitePythonShellPrivate *priv =
+        GTKPARASITE_PYTHON_SHELL_GET_PRIVATE(python_shell);
+
+    if (priv->cur_history_item == NULL || priv->cur_history_item->prev == NULL)
+    {
+        priv->cur_history_item = NULL;
+        return "";
+    }
+
+    priv->cur_history_item = priv->cur_history_item->prev;
+
+    return (const char *)priv->cur_history_item->data;
 }
 
 static gboolean
@@ -215,18 +298,14 @@ gtkparasite_python_shell_key_press_cb(GtkWidget *textview,
     }
     else if (event->keyval == GDK_Up)
     {
-#if 0
         gtkparasite_python_shell_replace_input(python_shell,
             gtkparasite_python_shell_get_history_back(python_shell));
-#endif
         return TRUE;
     }
     else if (event->keyval == GDK_Down)
     {
-#if 0
         gtkparasite_python_shell_replace_input(python_shell,
             gtkparasite_python_shell_get_history_forward(python_shell));
-#endif
         return TRUE;
     }
     else if (event->string != NULL)
@@ -257,7 +336,8 @@ gtkparasite_python_shell_key_press_cb(GtkWidget *textview,
                                                   &selection_iter);
 
         if (cmp_start_insert == 0 && cmp_start_select == 0 &&
-            event->keyval == GDK_BackSpace)
+            (event->keyval == GDK_BackSpace ||
+             event->keyval == GDK_Left))
         {
             return TRUE;
         }
